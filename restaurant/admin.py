72fd80423f9b707afaddbab0f3eaef72d383890a
Gabriel Domingues <gabrieldominguesconsultoria@gmail.com>
2024-09-09 23:33:50 -0300

    Inserção dos serializers

--- restaurant/admin.py
+++ restaurant/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from restaurant.models import Ingredient, MenuItem, RecipeRequirement, Purchase
+
+admin.site.register(Ingredient)
+admin.site.register(MenuItem)
+admin.site.register(RecipeRequirement)
+admin.site.register(Purchase)
+
--- restaurant/models.py
+++ restaurant/models.py
@@ -1,19 +1,53 @@
 from django.db import models
+from rest_framework import serializers
+
 
 class Ingredient(models.Model):
     name = models.CharField(max_length=100)
     quantity = models.IntegerField()
     price_per_unit = models.FloatField()
 
+    def __str__(self):
+        return self.name
+
 class MenuItem(models.Model):
     name = models.CharField(max_length=100)
     price = models.FloatField()
+
+    def __str__(self):
+        return self.name
 
 class RecipeRequirement(models.Model):
     menu_item = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
     ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
     quantity = models.FloatField()
 
+    def __str__(self):
+        return f"{self.menu_item} - {self.ingredient} - {self.quantity}"
+
 class Purchase(models.Model):
     menu_item = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
     timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.menu_item} - {self.timestamp.strftime('%Y-%m-%d %H:%M:%S')}"
+
+class MenuItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = MenuItem
+        fields = ['name', 'price']
+
+class IngredientSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Ingredient
+        fields = ['name', 'quantity', 'price_per_unit']
+
+class RecipeRequirementSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = RecipeRequirement
+        fields = ['menu_item', 'ingredient', 'quantity']
+
+class PurchaseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Purchase
+        fields = ['menu_item', 'timestamp']
